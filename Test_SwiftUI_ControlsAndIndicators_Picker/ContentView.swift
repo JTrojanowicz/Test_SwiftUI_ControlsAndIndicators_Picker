3be068751a347dd9c6e95c690ededcf1bf0e5d23
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-09-08 09:48:19 +0200

    Initial Commit

---
+++ Test_SwiftUI_ControlsAndIndicators_Picker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Test_SwiftUI_ControlsAndIndicators_Picker
+//
+//  Created by Jaroslaw Trojanowicz on 08/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test_SwiftUI_ControlsAndIndicators_Picker/Test_SwiftUI_ControlsAndIndicators_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_ControlsAndIndicators_PickerApp.swift
+//  Test_SwiftUI_ControlsAndIndicators_Picker
+//
+//  Created by Jaroslaw Trojanowicz on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_ControlsAndIndicators_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
